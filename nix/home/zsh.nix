1a5f11f2067f531b1acb767a89bc3d834b9c2ca4
arairyus <arai.ryusei.jp@gmail.com>
2026-07-14 22:11:25 +0900

    Revert "fix(zsh): add Homebrew PATH so az (installed via brew) is found"
    This reverts commit cd67fbc9c35e1f3febe916e93a5561c6b635031f.

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -33,13 +33,6 @@ in
       export GOENV_ROOT="$HOME/.goenv"
       export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#888888"
 
-      # Homebrew (azure-cli)
-      if [ -x /opt/homebrew/bin/brew ]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      elif [ -x /usr/local/bin/brew ]; then
-        eval "$(/usr/local/bin/brew shellenv)"
-      fi
-
       # PATH
       export PATH="$HOME/.local/bin:$PATH"
       export PATH="$HOME/dotfiles/bun/node_modules/.bin:$PATH"
